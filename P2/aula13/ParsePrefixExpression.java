99c989a31f6f6501ff20a9d98e4555711eeb706e
Bruno-SLopes <bruno25@ua.pt>
2020-05-22 02:09:04 +0100

    Create ParsePrefixExpression.java

---
+++ P2/aula13/ParsePrefixExpression.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class ParsePrefixExpression
+{
+  public static void main(String[] args) {
+    // ...
+
+  }
+
+  // ...
+
+}
