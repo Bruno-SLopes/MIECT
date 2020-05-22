7d421047d92e76963f2ec88bba0d565144c98b53
Bruno-SLopes <bruno25@ua.pt>
2020-05-22 01:39:48 +0100

    Create Fibonacci.java

---
+++ P2/aula6/Fibonacci.java
@@ -0,0 +1,35 @@
+import static java.lang.System.*;
+
+public class Fibonacci {
+  final static int array[] = new int[50]; 
+  public static void main(String[] args) {
+    if (args.length < 1) {
+      out.println("USO: java -ea Fibonacci N [N ...]");
+      exit(1);
+    }
+
+    // Alguns testes:
+    assert fibonacci(0) == 0;
+    assert fibonacci(1) == 1;
+    assert fibonacci(2) == 1;
+    assert fibonacci(5) == 5;
+
+    for(int i = 0; i < args.length; i++) {
+      int n = Integer.parseInt(args[i]);
+      long time = nanoTime();  // System.nanoTime() gives the time in ns.
+      int f = fibonacci(n);
+      time = nanoTime() - time;
+      out.printf("fibonacci(%d) = %d (%d ns)\n", n, f, time);
+    }
+  }
+
+  public static int fibonacci(int n) {
+    assert n >= 0;
+    
+    if(n <= 1) array[n] = n;
+
+    else if(array[n] == 0) array[n] = fibonacci(n-2) + fibonacci(n-1);
+    return array[n];
+  }
+
+}
